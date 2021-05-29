5bf5f3874f6a27ab96b40ad3ff85ea542a0a01e2
CameronChildress <cameronchildress4@gmail.com>
2021-05-29 13:27:18 -0600

    Added Hologram gun and added sloppy basic movement
    Added Hologram gun and added sloppy basic movement

--- Assets/BasicMovement.cs
+++ Assets/BasicMovement.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
             transform.position = new Vector3(transform.position.x + 0.05f, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x - 0.05f, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.05f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.05f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
         }
     }
 }
